125810ead51fb7711251bb94382950ad082b19d7
Mukanahallipathna Satish Kedarnag <kedarnag_mukanahalli_from.tebs@stb.gov.sg>
2019-08-12 13:52:40 +0800

    Updates

---
+++ precache-manifest.a9e7e3f77526ba294087dddbe5e4d948.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d83a0e61cbd73519cdd5eed726283d46",
+    "url": "/react-users/index.html"
+  },
+  {
+    "revision": "4cc920d1b13b9c4337d8",
+    "url": "/react-users/static/css/main.9dea4597.chunk.css"
+  },
+  {
+    "revision": "4b5e8ba200496ba0c0c9",
+    "url": "/react-users/static/js/2.f860d58a.chunk.js"
+  },
+  {
+    "revision": "4cc920d1b13b9c4337d8",
+    "url": "/react-users/static/js/main.b3b05d67.chunk.js"
+  },
+  {
+    "revision": "4bc0961c803e2576abf7",
+    "url": "/react-users/static/js/runtime~main.fd35e43a.js"
+  }
+]);
